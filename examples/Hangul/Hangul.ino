e5c235c95c544a031241e70ea3c0e068e7ef6c87
Unseelie <emunseelie@gmail.com>
2021-07-05 12:00:11 -0500

    improved explanation of the example.

--- examples/Hangul/Hangul.ino
+++ examples/Hangul/Hangul.ino
@@ -1,6 +1,11 @@
 /*This example wraps libhangul and includes a function get_arduino_char() that switches 
  * the output of the preedit strings and commit strings produced theirin from unicode codepoints
- * to utf8 codepoints so that we can print the characters on arduino based devices. 
+ * to utf8 codepoints so that we can print the characters on arduino based devices.
+ * this example prints to the serial information that the function would ultimately be passing to a text renderer:
+ * 1: the characther to display
+ * 2: whether a character is final (and thus the renderer should advance to a new cell)
+ * 3: whether a space has been intercepted (and thust the renderer ought place a space)
+ *
  *
  *Hangul text entry is more complicated than engilsh because the characters which 
  *comprise a sylable get stacked atop one another, rather than being strung along linearly.
@@ -49,13 +54,13 @@ void loop()
       isfinal = get_arduino_char(hic,input[index], output);
       if(isfinal==true)
       {
-        result = result + output;
+        result = result + output;//in our case, result is rendering the final string as it is formed. 
       }
       if(check_if_space == 1){
         result = result+' ';
       }
       Serial.printf("the char is final: %d, there was a space %d, the char is:", isfinal, check_if_space);
-      Serial.println(output); 
+      Serial.println(output); //rendering characters as they ought be displayed in sequence with finality denoted. 
   }
     
 
