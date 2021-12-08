2de6ae56eb40a45bb9fdee90a590b4911b55f91b
Jessica9-star <68959096+Jessica9-star@users.noreply.github.com>
2021-12-08 14:29:39 -0600

    Update keyinput

--- examples/KeyInput/keyinput.ino
+++ examples/KeyInput/keyinput.ino
@@ -1,6 +1,6 @@
 // this is for the hangul wrapper
 #include <hangul.h>
-#include <cassert>
+//#include <cassert>
 #include <inttypes.h>
 
 
@@ -26,43 +26,31 @@ bool get_arduino_char1(HangulInputContext *hic, char keystroke, char *output)
  * *output, passed in as an output parameter, now filled with a character. 
 */
 {
-  Serial.print("  getarduinoCharkeystroke: ");
-  Serial.print(keystroke);
   int ret = hangul_ic_process(hic, keystroke);//handles backspaces and keyboards
   uint8_t utf8[4]={0,0,0,0};
   uint8_t pre_edit[4] = {0,0,0,0};
   char* single;
   int ascii;
-  Serial.println("test1");
 
   const ucschar *commit_string = hangul_ic_get_commit_string(hic); //get the preedit strings by passing characters (simulating keystrokes into the get_string functions
   if (*commit_string == 0)
   {
-    Serial.println("test2a");
     const ucschar *preedit_string = hangul_ic_get_preedit_string(hic);
     unsigned len2 = unicode_codepoint_to_utf8(pre_edit, *preedit_string);
     
     
     single = ((char *)pre_edit);
-    Serial.println("test3");
-    Serial.print("single: ");
-    Serial.println(*single);
     memcpy(output, single, 32);
-    
-    Serial.println("test4");
-    
-    Serial.print(*output);
+        
     return false;
     }
   else
   {
-    Serial.println("test2b");
     unsigned len = unicode_codepoint_to_utf8(utf8, *commit_string);//convert the uschars to utf8 ecoded uint8_t[4] arrays. 
     single = (char *)utf8;
     
        
     memcpy(output, single, 32);
-    Serial.print(*output);
     return true;
     
     }
@@ -72,25 +60,23 @@ bool get_arduino_char1(HangulInputContext *hic, char keystroke, char *output)
  }
 
 
-void generateLocalBuffer(char keystroke, String output)
+String generateLocalBuffer(char keystroke, String output)
 {
-  char *arduinoChar;
-  Serial.print("keystroke in generateLocalBuffer: ");
-  Serial.println(keystroke);
+  char arduinoChar[32];
+
   bool isFinal = get_arduino_char1(hic, keystroke, arduinoChar);
-  Serial.print(isFinal);
-  Serial.println("arduinoHangul: ");
-  Serial.println(arduinoChar);
   if (isFinal == true)
   {
-    localBuffer += output;
-//    strcat(localBuffer, output);
+    //localBuffer += output;
+    localBuffer = String(localBuffer + arduinoChar);
+    return localBuffer;
   }
   else
   {
     localBufferA = localBuffer;
-    localBufferA += output;
-//    strcat(localBufferA, output);
+    //localBufferA += output;
+    localBufferA = String(localBuffer + arduinoChar);
+    return localBufferA;
   }
 }
   
@@ -101,16 +87,10 @@ void setup()
   
 void loop()   
 {
-   while (!Serial);
-   Serial.println("display buffer: ");
    if (Serial.available() > 0){//checks for user input
      String textInputString = Serial.readString();
-     Serial.print("textInputString: ");
-     Serial.println(textInputString);
      char textInputChar = textInputString.charAt(0);
-     Serial.print("textInputChar: ");
-     Serial.println(textInputChar);
-     generateLocalBuffer(textInputChar, localBuffer);
-     Serial.println(localBuffer);
+     String composeBuffer = generateLocalBuffer(textInputChar, localBuffer);
+     Serial.println(composeBuffer);
    }
 } 
