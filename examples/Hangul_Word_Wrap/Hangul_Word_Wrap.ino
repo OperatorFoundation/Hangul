d71a75be2ca61ea1d63fad9963e09fc7e3a486c6
Unseelie <emunseelie@gmail.com>
2021-12-17 08:18:44 -0500

    sub-functions hopefully clear up the word wrap example

--- examples/Hangul_Word_Wrap/Hangul_Word_Wrap.ino
+++ examples/Hangul_Word_Wrap/Hangul_Word_Wrap.ino
@@ -21,12 +21,10 @@
  * libhangul decides whether characters are two or three jamo long based on a Lead, vowel, Tail pattern 
  * that exists in the korean language. 
  * so after the machine has seen enough of the pattern (in this case, four characters), as you enter keys vkdl
- * it will display ㅍ 파 팡 파이 in sequence. Once itknows to move on to a new key, it has a commit string and a new pre-edit string. 
- * 
- * This example includes the functions generateLocalBuffer(), test(), and returnHangulText(). 
- * Of these, generateLocalBuffer runs a loop on input text to convert the keys to the correct symbols, test() allows for inputting
- * test cases, and returnHangulText() wraps generateLocalBuffer to make it very easy to use (give returnHangulText a string of English
- * Keystrokes, and it will return the Hangul localization). 
+ * it will display ㅍ 파 팡 파이 in sequence. Once it knows to move on to a new cell, it has a commit string and pushes that as a final character, and moves on to constructing a new pre-edit string. 
+ * by following that process, it builds characters in 1s, 2s, or 3s, based on the pattern. 
+ *
+ * this example includes composeBufferToDisplayLocalLang(), a function that calls wrapOnSpaces and wrapNoSpaces to handle word-wrapping on a global variable composeBuffer. But the execution loop cheats by adding a trailing space to every string, rather than including any handling for the last word in the segment. 
 */
 
 #include <hangul.h>
@@ -44,9 +42,10 @@ String correct3 = "녕ㅆ";
 String input4 = "s u d";
 String correct4 = "ㄴ ㅕ ㅇ";
 
-String inputBig = "vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm";
-String correctBig = "파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트";
+String inputBig = "vkdlTjsgksrmffkdlqmfjflxptmxmvkdlTjsgksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm vkdlTjs gksrmf fkdlqmfjfl xptmxm";
+String correctBig = "파이썬한글라이브러리테스트파이썬한글라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트 파이썬 한글 라이브러리 테스트";
 
+String composeBuffer = "";
 
 void setup() {
     Serial.begin(9600);
@@ -61,15 +60,12 @@ static int ucscharlen(const ucschar *str)
     return end - str;
 }
 
-String composeBuffer = "";
-void composeBufferToDisplayLocalLang(){
-  
-    bool check_if_space;
+void wrapNoSpaces(int indexStart){
     bool isfinal;
     const char* keyboard = "2";
     String currentWord= "";
     int8_t charWidth = 8;
-    int maxCharPerLine = 30/int(charWidth);
+    int maxCharPerLine = 20/int(charWidth);
     String currentLineBuffer = "";
     int wordSize = 0;
     int positionOnLine = 0;
@@ -77,103 +73,101 @@ void composeBufferToDisplayLocalLang(){
     char output[32];
     HangulInputContext *hic = hangul_ic_new(keyboard); //Brandon says never optimize. This feels very unoptimal. 
     String result = "";
+    
+    for(int index=indexStart; index <= composeBuffer.length(); index++){     
+      isfinal = get_arduino_char(hic,composeBuffer[index], output);
+      if(isfinal==true){
+        currentWord += output;//in our case, result is rendering the final string as it is formed.
+        wordSize += 1;
+      }
+      if(wordSize+positionOnLine > maxCharPerLine){//if we exceed the symbol limit on 
+        currentWord = "\n" + currentWord +" ";      //this line, we word wrap, appending
+        Serial.print(currentWord);                  //a new line, the word, and a space. 
+        result += currentWord;
+        //bookeeping to reset the word and line sizes
+        currentWord = "";
+        positionOnLine = wordSize+1;
+        wordSize = 0; 
+        }    
+      //Serial.printf("the char is final: %d, there was a space %d, the char is:", isfinal, check_if_space);
+      //Serial.println(output); //rendering characters as they ought be displayed in sequence with finality denoted. 
+  }
+  Serial.print(currentWord);//print any characters remaining after that last space. 
+}
 
-    int lastSpaceIndex = composeBuffer.lastIndexOf(' ');
+void wrapOnSpaces(){
+        bool check_if_space;
+        bool isfinal;
+            const char* keyboard = "2";
+    String currentWord= "";
+    int8_t charWidth = 8;
+    int maxCharPerLine = 100/int(charWidth);
+    Serial.printf("max chars: %d \n",maxCharPerLine);
+    String currentLineBuffer = "";
+    int wordSize = 0;
+    int positionOnLine = 0;
     
-    if(lastSpaceIndex > 1){//there are spaces, so we can primarily wrap on those. 
-      for(int index=0; index <= composeBuffer.length(); index++)
-      {
-          //check if space outside of get_Arduino_char!!!!!!!!!
-          check_if_space = handle_spaces(composeBuffer[index]);
-          
-          isfinal = get_arduino_char(hic,composeBuffer[index], output);
-          if(isfinal==true)
-          {
-            currentWord += output;//in our case, result is rendering the final string as it is formed.
-            wordSize += 1;
-             
-          }
-          if(check_if_space == 1){ //
-             if(wordSize+positionOnLine > maxCharPerLine){//if we exceed the symbol limit on 
-              currentWord = "\n" + currentWord +" ";      //this line, we word wrap, appending
-              Serial.print(currentWord);                  //a new line, the word, and a space. 
-              result += currentWord;
-              //bookeeping to reset the word and line sizes
-              currentWord = "";
-              positionOnLine = wordSize+1;
-              wordSize = 0; 
-            } else{ //add a space only if we don't word wrap. 
-              currentWord += " ";
-              wordSize += 1;
-            }
-           
-          }
-          //Serial.printf("the char is final: %d, there was a space %d, the char is:", isfinal, check_if_space);
-          //Serial.println(output); //rendering characters as they ought be displayed in sequence with finality denoted. 
-     
-      }
-    Serial.print(currentWord);
-    //print any characters remaining after that last space. 
-    } else if(lastSpaceIndex == -1){//there are no spaces, so we cannot depend on spaces to print.  
-      for(int index = 0; index <= composeBuffer.length(); index++)
-      {
+    char output[32];
+    HangulInputContext *hic = hangul_ic_new(keyboard);  
+    String result = "";
+        for(int index=0; index <= composeBuffer.length(); index++)
+        {
+        if(wordSize == maxCharPerLine){
+          Serial.println(currentLineBuffer);//first, dump the line)
+          currentLineBuffer = currentWord; //then make the line the too-long word
+          Serial.println(currentLineBuffer);
+          currentLineBuffer="";
+          currentWord = "";
+          positionOnLine = 0;
+          wordSize = 0;
+        }
+        check_if_space = handle_spaces(composeBuffer[index]);
         
-        isfinal = get_arduino_char(hic,composeBuffer[index], output);
-        Serial.printf("the char is final: %d, index is %d, the char is:", isfinal, index);
-        Serial.println(output); //rendering characters as they ought be displayed in sequence with finality denoted. 
-      }
-    
-//        if(isfinal==true)
-//        {
-//          currentWord += output;//in our case, result is rendering the final string as it is formed.
-//          positionOnLine += 1;
-//          if(positionOnLine >= maxCharPerLine){
-//            currentWord = currentWord+'\n';
-//            Serial.print(currentWord);
-//            positionOnLine = 0;
-//            result += currentWord;
-//            currentWord = "";
-//            
-//          }
-//          
-//        }else{//is final is false, so check if we're at the end of our string. 
-//          if(index = composeBuffer.length()-1){ //if the last character isn't a final character...run with it!
-//            Serial.printf("buffer is %d long\n", composeBuffer.length());
-//            Serial.printf("we are at the %dnth character of the word\n", index);
-//            
-//            currentWord += output;//
-//            positionOnLine += 1;
-//            if(positionOnLine >= maxCharPerLine){
-//              currentWord = currentWord+'\n';
-//              Serial.print(currentWord);
-//              positionOnLine = 0;
-//              result += currentWord;
-//              currentWord = "";
-//            }
-//            Serial.print(currentWord);
-//          }
-//        }
-//      }
-//      Serial.print(currentWord);
-    }else{//start at the last space, do the rest of the chars. 
-      for(int index = lastSpaceIndex; index < composeBuffer.length(); index++){
-      {
         isfinal = get_arduino_char(hic,composeBuffer[index], output);
         if(isfinal==true)
         {
-          currentWord += output;//in our case, result is rendering the final string as it is formed.
-          positionOnLine += 1;
-          if(positionOnLine >= maxCharPerLine){
-            currentWord = currentWord+'\n';
-            Serial.print(currentWord);
-            positionOnLine = 0;
-            result += currentWord;
-            currentWord = "";
-          }
+          currentWord += output;//
+          wordSize += 1;
+          if(wordSize> maxCharPerLine){ //we must segment the word
+              //currentWord += "\n";
+              currentLineBuffer+=currentWord;
+              currentWord = "";
+              positionOnLine = wordSize;
+              wordSize = 0;
+          }       
+           
         }
-      }
-    } 
+        if(check_if_space == 1){ //
+          //building a word and we found a space. can either append to the line or start a new line. 
+                 
+           if(wordSize+positionOnLine > maxCharPerLine){//our word doesn't fit, so start a new line
+            Serial.println(currentLineBuffer); //dump the buffer
+            currentLineBuffer = currentWord+" "; //empty the buffer and fill our new word
+            positionOnLine = wordSize+1;
+            currentWord = "";//empty the word
+            wordSize = 0;
+            
+           } else { //we add the word to the line. 
+            currentLineBuffer += currentWord + " ";
+            currentWord = "";
+            positionOnLine += wordSize+1;
+            wordSize = 0;
+          }
+        } 
+    }
+    Serial.println(currentLineBuffer);//print any characters remaining after that last space. 
   }
+
+
+
+void composeBufferToDisplayLocalLang(){
+    int lastSpaceIndex = composeBuffer.lastIndexOf(' ');
+    if(lastSpaceIndex > 0){//there are spaces, so we can primarily wrap on those. 
+      wrapOnSpaces();
+      //wrapNoSpaces(lastSpaceIndex);//to print the last word
+    } else if(lastSpaceIndex == -1){//there are no spaces, so we cannot depend on spaces to print.  
+      wrapNoSpaces(0);
+    }   
 }
 
 void loop(){
@@ -183,42 +177,42 @@ void loop(){
 
   Serial.println("++++++++++++++Test 1+++++++++++++++");
   Serial.print("output: ");
-  composeBuffer = input1;
+  composeBuffer = input1+ " ";
   composeBufferToDisplayLocalLang();
   Serial.print("\nun-wrapped text: ");
   Serial.println(correct1);
   
   Serial.println("++++++++++++++Test 2+++++++++++++++");
   Serial.print("output: ");
-  composeBuffer = input2;
+  composeBuffer = input2+ " ";
   composeBufferToDisplayLocalLang();
   Serial.print("\nun-wrapped text: ");
   Serial.println(correct2);
 
   Serial.println("++++++++++++++Test 3+++++++++++++++");
   Serial.print("output: ");
-  composeBuffer = input3;
+  composeBuffer = input3+ " ";
   composeBufferToDisplayLocalLang();
   Serial.print("\nun-wrapped text: ");
   Serial.println(correct3);
 
   Serial.println("++++++++++++++Test 4+++++++++++++++");
   Serial.print("output: ");
-  composeBuffer = input4;
+  composeBuffer = input4+ " ";
   composeBufferToDisplayLocalLang();
   Serial.print("\nun-wrapped text: ");
   Serial.println(correct4);
 
 //  Serial.println("++++++++++++++Test big+++++++++++++++");
 //  Serial.print("output: ");
-//  composeBuffer = inputBig;
+//  composeBuffer = inputBig+ " ";
 //  timestamp1 = millis();
 //  composeBufferToDisplayLocalLang();
 //  timestamp2 = millis();
 //  Serial.print("\nun-wrapped text: ");
 //  Serial.println(correctBig);
 //  Serial.printf("big test duration: %d", timestamp2-timestamp1); 
-  
+//  
   delay(10000);
   Serial.println();
 }
